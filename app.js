66e7424abe7be37dfdc86d3f668df4bae4bcd977
Luiz Fernando <luizfernandorg@gmail.com>
2021-10-22 05:22:34 -0300

    set up the server

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express();
 app.get("/api", (req, res) => {
     res.send("Welcome to the timestamp api");
 })
-
-app.listen(() => {
+var port = process.env.PORT || 3000;
+app.listen(port,() => {
     console.log("Lisining...");
 })
