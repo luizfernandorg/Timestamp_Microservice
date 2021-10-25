8104b242576e4a7ec553424cd09d7319b282cd0a
Luiz Fernando <luizfernandorg@gmail.com>
2021-10-24 22:14:43 -0300

    Added new path that handle no input date

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const app = express();
 
+app.get("/api", (req,res) => {
+    let _date = new Date();
+    res.json({"unix": _date.getTime(),"utc":_date.toUTCString()});
+});
+
 app.get("/api/[0-9]{4}\-[0-9]{2}\-[0-9]{2}", (req,res) => {
     let _date = new Date(req.url.substr(5,req.url.length));
     res.json({"unix": _date.getTime(),"utc":_date.toUTCString()});
