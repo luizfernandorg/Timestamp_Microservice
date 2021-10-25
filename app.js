6bc8e76c61ac3ef078e08cff81e9ec919174497a
Luiz Fernando <luizfernandorg@gmail.com>
2021-10-24 23:26:59 -0300

    Update path now has one path that handles all possibilities

--- app.js
+++ app.js
@@ -1,24 +1,23 @@
 const express = require("express");
 const app = express();
 
-app.get("/api", (req,res) => {
-    let _date = new Date();
-    res.json({"unix": _date.getTime(),"utc":_date.toUTCString()});
-});
-
-app.get("/api/[0-9]{4}\-[0-9]{2}\-[0-9]{2}", (req,res) => {
-    let _date = new Date(req.url.substr(5,req.url.length));
-    res.json({"unix": _date.getTime(),"utc":_date.toUTCString()});
-});
-
-app.get("/api/[0-9]*", (req, res) => {
-    try {
-        let _date = new Date((parseInt(req.url.substr(5,req.url.length))/1000)*1000);
+app.get("/api/:date?", (req,res) => {
+    const _param = req.params.date;
+    const fdate = /^[0-9]{4}\-[0-9]{2}\-[0-9]{2}$/;
+    const udate = /^[0-9]{13}$/;
+    console.log({ "msg" : req.params.date });
+    if(_param === undefined){
+        const _date = new Date();
         res.json({"unix": _date.getTime(),"utc":_date.toUTCString()});
-    } catch (e){
+    } else if(_param.match(fdate)){
+        const _date = new Date(_param);
+        res.json({"unix": _date.getTime(),"utc":_date.toUTCString()});
+    } else if(_param.match(udate)){
+        const _date = new Date((parseInt(_param)/1000)*1000);
+        res.json({"unix": _date.getTime(),"utc":_date.toUTCString()});
+    } else {
         res.json({ error : "Invalid Date" })
     }
-    
 });
 
 var port = process.env.PORT || 3000;
