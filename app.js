0fba27c527eca6e2ce7f24b19259addb7416b48e
Luiz Fernando <luizfernandorg@gmail.com>
2021-10-24 22:27:21 -0300

    update routes to handle invalid date stamp

--- app.js
+++ app.js
@@ -10,10 +10,17 @@ app.get("/api/[0-9]{4}\-[0-9]{2}\-[0-9]{2}", (req,res) => {
     let _date = new Date(req.url.substr(5,req.url.length));
     res.json({"unix": _date.getTime(),"utc":_date.toUTCString()});
 });
-app.get("/api/[0-9]{13}", (req, res) => {
-    let _date = new Date((parseInt(req.url.substr(5,req.url.length))/1000)*1000);
-    res.json({"unix": _date.getTime(),"utc":_date.toUTCString()});
-})
+
+app.get("/api/[0-9]*", (req, res) => {
+    try {
+        let _date = new Date((parseInt(req.url.substr(5,req.url.length))/1000)*1000);
+        res.json({"unix": _date.getTime(),"utc":_date.toUTCString()});
+    } catch (e){
+        res.json({ error : "Invalid Date" })
+    }
+    
+});
+
 var port = process.env.PORT || 3000;
 app.listen(port,() => {
     console.log(`Lisining on ${port}`);
