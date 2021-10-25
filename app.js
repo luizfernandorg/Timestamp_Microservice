b489e7278afacc2ee259a562a90b324fa01b8d94
Luiz Fernando <luizfernandorg@gmail.com>
2021-10-25 00:05:40 -0300

    last update on the path /api/:date?

--- app.js
+++ app.js
@@ -3,9 +3,7 @@ const app = express();
 
 app.get("/api/:date?", (req,res) => {
     const _param = req.params.date;
-    /**
-     * TODO: validate date in the format "25 october 2015"
-     */
+    const sdate = /^[0-9]{2}\ [a-zA-Z]*\ [0-9]{4}$/
     const fdate = /^[0-9]{4}\-[0-9]{2}\-[0-9]{2}$/;
     const udate = /^[0-9]{13}$/;
     console.log({ "msg" : req.params.date });
@@ -18,6 +16,10 @@ app.get("/api/:date?", (req,res) => {
     } else if(_param.match(udate)){
         const _date = new Date((parseInt(_param)/1000)*1000);
         res.json({"unix": _date.getTime(),"utc":_date.toUTCString()});
+    } else if(_param.match(sdate)){
+      const _date = new Date(_param)|| "";
+      if(_date == "") res.json({ error : "Invalid Date" })
+      res.json({"unix": _date.getTime(),"utc":_date.toUTCString()});
     } else {
         res.json({ error : "Invalid Date" })
     }
